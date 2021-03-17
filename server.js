be779b08e4f67dd1e5650f45805b147c694e4068
HS <ktkhs1435@gmail.com>
2021-03-17 16:40:08 +0900

    add get customer rest api module

--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'Hello express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+            {
+              'id': 1,
+              'image' : 'https://placeimg.com/64/64/1',
+              'name' : '홍길동',
+              'birthday' : '961222',
+              'gender' : '남자',
+              'job': '대학생'
+            },
+          
+            {
+              'id': 2,
+              'image' : 'https://placeimg.com/64/64/2',
+              'name' : '동빈나 ',
+              'birthday' : '961222',
+              'gender' : '여성',
+              'job': '대학생'
+            },
+          
+            {
+              'id': 3,
+              'image' : 'https://placeimg.com/64/64/3',
+              'name' : '빈나 ',
+              'birthday' : '921222',
+              'gender' : '여성',
+              'job': '대학생'
+            }
+          ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
